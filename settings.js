151979c81b4466d61fedac8c0dac12c4c6e5a3f3
Kingkaviya13 <126646806+Kingkaviya13@users.noreply.github.com>
2024-04-07 12:08:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
+  OWNER_NUMBER: "94769672030",
+  SUDO_NUMBERS : "94769672030",
+  OWNER_NAME: "『 𝙼𝚁 KAVIIX 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
